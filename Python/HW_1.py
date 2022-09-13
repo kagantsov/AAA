e69e2cbfbd8bec133f0e8febee532a0b1e4da526
PC <gkagantsov@mail.ru>
2022-09-13 15:40:03 +0300

    HW1

---
+++ Python/HW_1.py
@@ -0,0 +1,31 @@
+def step1():
+    print(
+        'Утка-маляр 🦆 решила выпить зайти в бар. '
+        'Взять ей зонтик? ☂️'
+    )
+    option = ''
+    options = {'да': True, 'нет': False}
+    while option not in options:
+        print('Выберите: {}/{}'.format(*options))
+        option = input()
+
+    def step2_umbrella():
+        print(
+            'Какое небо голубое '
+            'И светлый чистый горизонт '
+            'Все потому что я сегодня '
+            'Взял зонт '
+        )
+
+    def step2_no_umbrella():
+        print(
+            'Зря, зря - the duck said'
+        )
+
+    if options[option]:
+        return step2_umbrella()
+    return step2_no_umbrella()
+
+
+if __name__ == '__main__':
+    step1()
